77e41d6e3b413818b052fa608ae15d2a3d52fbfd
douzihedouya <zhhhfu@163.com>
2019-08-07 17:17:41 +0800

    新增希尔排序，但是可能有问题

--- src/MajorAtProblemTest/SortTest.java
+++ src/MajorAtProblemTest/SortTest.java
@@ -3,13 +3,15 @@ package MajorAtProblemTest;
 import SortAlgorithm.BulubulubuluSort;
 import SortAlgorithm.InsertionSort;
 import SortAlgorithm.SelectionSort;
+import SortAlgorithm.ShellSort;
 
 public class SortTest {
     public static void main(String[] args){
         int[] arr = {45,34,56,39,45,5,67,87,25,7};
         //new SelectionSort().sort(arr);
         //new BulubulubuluSort().sort(arr);
-        new InsertionSort().sort(arr);
+        //new InsertionSort().sort(arr);
+        new ShellSort().sort(arr);
         for (int after:arr) {
             System.out.println(after);
         }
---
+++ src/SortAlgorithm/ShellSort.java
@@ -0,0 +1,27 @@
+package SortAlgorithm;
+
+public class ShellSort extends SortAlgorithm {
+    /**
+     * 希尔排序是选择排序的升级版
+     * 不同于选择排序，希尔排序是对间隔为h的元素进行排序，使其大体有序
+     * 然后对选择间隔不断进行减小，最终为1时再排序。
+     * 我的方法与网上方法有明显不同，不知道孰优孰劣。
+     * @param num 待排序数组
+     */
+    @Override
+    public void sort(int[] num) {
+        int length = num.length;
+        int h=1;
+        while(h<length/3){
+            h = 3*h+1;
+        }
+        while (h>=1){
+            for (int i=length-1;i-h>=0;i--){
+                if (num[i]<num[i-h]){
+                    swap(num, i,i-h);
+                }
+            }
+            h = h-1;
+        }
+    }
+}
